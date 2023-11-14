0436710b165b46d644fbc543fe993acd83642b53
mindkeeper <fcb.nyak@gmail.com>
2023-11-14 22:36:50 +0700

    initial commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,39 @@
+'use-client';
+
+import Image from 'next/image';
+import { ModeToggle } from '../core/theme-toggle';
+
+function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li>
+            {' '}
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className="dark:invert"
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </li>
+          <li>
+            <ModeToggle />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
